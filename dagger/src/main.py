44157635525f54a48a0e8da40245e07b7a853148
Helder Correia <174525+helderco@users.noreply.github.com>
2024-02-19 11:21:20 -0100

    Initial commit
    Signed-off-by: Helder Correia <174525+helderco@users.noreply.github.com>

---
+++ dagger/src/main.py
@@ -0,0 +1,17 @@
+import dagger
+from dagger import dag, field, function, object_type
+
+
+@object_type
+class CustomSdk:
+    sdk_source_dir: dagger.Directory
+    required_paths: list[str] = field(default=list)
+
+    @function
+    def module_runtime(self, mod_source: dagger.ModuleSource, introspection_json: str) -> dagger.Container:
+        return dag.python_sdk(sdk_source_dir=self.sdk_source_dir).module_runtime(mod_source, introspection_json)
+
+    @function
+    def codegen(self, mod_source: dagger.ModuleSource, introspection_json: str) -> dagger.GeneratedCode:
+        return dag.python_sdk(sdk_source_dir=self.sdk_source_dir).codegen(mod_source, introspection_json)
+
